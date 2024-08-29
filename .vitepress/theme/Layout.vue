3f4dd85fcfc62b5d1f9d4871f7d68dddb8a5d5e7
JJoshMa <3068629642@qq.com>
2024-08-30 01:15:03 +0800

    添加导航栏

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,23 +1,54 @@
 <script setup lang="ts">
 import {Content, useData} from 'vitepress'
+import {ref} from "vue";
 
 const data = useData()
-const {site, frontmatter} = data
-console.log(data.localeIndex.value)
+const {site} = data
+
+const navList = ref(['root'])
+
+document.addEventListener('click', (e) => {
+  const fn = (i: number) => {
+    if (i === 0)
+      return;
+
+    const currentTitle = data.page.value.title
+    if (currentTitle === '') {
+      setTimeout(() => fn(--i), 50)
+      return;
+    }
+    if (navList.value[navList.value.length - 1] === currentTitle) {
+      return
+    }
+
+    for (let i = 0; i < navList.value.length; i++) {
+      if (currentTitle === navList.value[i]) {
+        navList.value = navList.value.splice(0, i + 1)
+        return;
+      }
+    }
+
+    navList.value.push(data.page.value.title)
+  }
+
+  setTimeout(() => fn(20), 50)
+})
+
 </script>
 
 <template>
   <img src="../../assets/DragonMedium.png" class="dragon-image" alt="404"/>
-  <div >
+  <div>
     <a href="/">Home</a>
     <h1>{{ site.title }}</h1>
-    <a href="/compiler/编译原理">测试</a>
-    <a href="/api-examples">引用 </a>
-    
-    <div>{{frontmatter}}</div>
-    <div>{{data}}</div>
-    
-    <div style="padding-left: 24px;max-width: 90%" >
+    <a href="/compiler/编译原理">编译原理</a>
+
+    <a href=""> 123 </a>
+
+    <a href="/api-examples">examples </a>
+    <div>{{ navList }}</div>
+
+    <div style="padding-left: 24px;max-width: 90%">
       <Content/>
     </div>
   </div>
@@ -35,7 +66,7 @@ console.log(data.localeIndex.value)
 }
 
 a:visited {
-  color: #0000FF; 
+  color: #0000FF;
 }
 
 </style>
